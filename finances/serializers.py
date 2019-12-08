19d5a18c9eb3d55c0f5c9b819a8f83cccfc154e9
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2019-12-08 16:50:52 -0600

    Serialize membership model

--- finances/serializers.py
+++ finances/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import *
 
-class MembershipSerializer(serializers.HyperlinkedModelSerializer):
+class MembershipSerializer(serializers.ModelSerializer):
     class Meta:
         model = Membership
+        exclude = []
+
+class BankMovementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BankMovement
         exclude = []
