dbc9ad4c797b09d1a283ce82f9f31589cceb04bb
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2019-12-11 12:49:50 -0600

    Display model identifiers

--- productivity/admin.py
+++ productivity/admin.py
@@ -16,6 +16,7 @@ class AdminPartner(admin.ModelAdmin):
 class AdminLine(admin.ModelAdmin):
     list_display = ('topic',)
     list_filter = ('topic',)
+    list_per_page = 10
 
 @admin.register(Division)
 class AdminDivision(admin.ModelAdmin):
@@ -26,21 +27,31 @@ class AdminDivision(admin.ModelAdmin):
 class AdminProject(admin.ModelAdmin):
     list_display = ('title','start', 'end', 'responsible',)
     list_filter = ('responsible', 'start', 'end', 'lines')
+    list_per_page = 10
 
 @admin.register(Research)
 class AdminResearch(admin.ModelAdmin):
     list_display = ('title', "resume", "year", "type")
     list_filter = ('title', 'projects', 'lines', "year", "type")
     search_fields = ('title',)
+    list_per_page = 10
+
+@admin.register(Author)
+class AdminAuthor(admin.ModelAdmin):
+    list_display = ('member', 'position', 'research')
+    list_filter = ('member', 'position', 'research')
+    list_per_page = 10
 
 @admin.register(Advisor)
 class AdminAdvisor(admin.ModelAdmin):
     list_display = ('member', 'position', 'research')
-    list_filter = ('member', 'position',)
+    list_filter = ('member', 'position', 'research')
+    list_per_page = 10
 
 
 @admin.register(Member)
 class AdminMember(admin.ModelAdmin):
     list_display = ('name', 'surname', 'active', 'charge', 'adscription', 'thumbnailFile', 'thumbnailUrl')
     list_filter = ('active', 'divisions', 'charge', 'roles', 'adscription')
-    search_fields = ('name', 'charge',)
+    search_fields = ('name', 'surname', 'charge',)
+    list_per_page = 10
--- productivity/models.py
+++ productivity/models.py
@@ -6,10 +6,12 @@ from datetime import datetime, timedelta
 # Define Google Drive Storage
 gd_storage = GoogleDriveStorage()
 
+
 class Role(models.Model):
     def __str__(self):
         return self.name
     name = models.CharField(default="", max_length=100, unique=True)
+
 
 class Division(models.Model):
     def __str__(self):
@@ -24,7 +26,8 @@ class Partner(models.Model):
     def __str__(self):
         return self.alias
     uuid = models.UUIDField(default=uuid4, primary_key=True, editable=True)
-    name = models.CharField(default="", editable=True, max_length=255, unique=True)
+    name = models.CharField(default="", editable=True,
+                            max_length=255, unique=True)
     alias = models.CharField(max_length=100, blank=True)
     site = models.URLField(default="", max_length=100, blank=True)
     logoName = models.CharField(max_length=100, blank=True)
@@ -39,21 +42,26 @@ class Member(models.Model):
     name = models.CharField(default="", max_length=100)
     surname = models.CharField(default="", blank=True, max_length=100)
     email = models.EmailField(default="", blank=True, max_length=100)
-    divisions = models.ManyToManyField(Division, blank=True, verbose_name="divisions")
+    divisions = models.ManyToManyField(
+        Division, blank=True, verbose_name="divisions")
     active = models.BooleanField(default=False)
     board = models.BooleanField(default=False, blank=True)
-    thumbnailUrl = models.CharField(max_length=100, default="https://drive.google.com/uc?id=", blank=True)
+    thumbnailUrl = models.CharField(
+        max_length=100, default="https://drive.google.com/uc?id=", blank=True)
     thumbnailFile = models.ImageField(
         default=None, null=True, upload_to='thumbnail', storage=gd_storage)
-    roles = models.ManyToManyField(Role, blank=True, verbose_name="list of roles")
+    roles = models.ManyToManyField(
+        Role, blank=True, verbose_name="list of roles")
     charge = models.CharField(max_length=100, default="", blank=True)
-    adscription = models.ForeignKey(Partner, null=True, blank=True, related_name="adscription_institute", on_delete=models.SET_NULL)
-    
+    adscription = models.ForeignKey(
+        Partner, null=True, blank=True, related_name="adscription_institute", on_delete=models.SET_NULL)
+
 
 class Line(models.Model):
     def __str__(self):
         return self.topic
     topic = models.CharField(default="", unique=True, max_length=200)
+
 
 class Project(models.Model):
     def __str__(self):
@@ -63,19 +71,26 @@ class Project(models.Model):
     start = models.DateField(default=datetime.now, blank=True)
     end = models.DateField(default=datetime.now, blank=True)
     responsible = models.CharField(default="", max_length=100, blank=True)
-    collaborators = models.ManyToManyField(Member, blank=True, verbose_name="collaborators")
-    institute = models.ForeignKey(Partner, null=True, on_delete=models.SET_NULL)
-    lines = models.ManyToManyField(Line, blank=True, verbose_name="interest areas")
+    collaborators = models.ManyToManyField(
+        Member, blank=True, verbose_name="collaborators")
+    institute = models.ForeignKey(
+        Partner, null=True, on_delete=models.SET_NULL)
+    lines = models.ManyToManyField(
+        Line, blank=True, verbose_name="interest areas")
+
 
 class Research(models.Model):
     def __str__(self):
         return self.title
+
     class Meta:
         verbose_name_plural = "research"
     uuid = models.UUIDField(default=uuid4, primary_key=True, editable=True)
     title = models.TextField(default="", blank=False)
-    lines = models.ManyToManyField(Line, blank=True, verbose_name="research lines")
-    projects = models.ManyToManyField(Project, blank=True, verbose_name="related projects")
+    lines = models.ManyToManyField(
+        Line, blank=True, verbose_name="research lines")
+    projects = models.ManyToManyField(
+        Project, blank=True, verbose_name="related projects")
     resume = models.TextField(default="", blank=True)
     year = models.IntegerField(default=2018, blank=True)
     grade = models.CharField(default="", max_length=100, blank=True)
@@ -87,17 +102,21 @@ class Research(models.Model):
 
 
 class Advisor(models.Model):
+    def __str__(self):
+        return '{0}.- {1}, {2}.'.format(self.position, self.member.surname, self.member.name[0:1])
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    research = models.ForeignKey(Research, related_name="advisors", on_delete=models.CASCADE)
+    research = models.ForeignKey(
+        Research, related_name="advisors", on_delete=models.CASCADE)
     position = models.IntegerField(blank=True, default=1)
+
 
 class Author(models.Model):
+    def __str__(self):
+        return '{0}.- {1}, {2}.'.format(self.position, self.member.surname, self.member.name[0:1])
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    research = models.ForeignKey(Research, default=None, related_name="authors", on_delete=models.CASCADE)
+    research = models.ForeignKey(
+        Research, default=None, related_name="authors", on_delete=models.CASCADE)
     position = models.IntegerField(blank=True, default=1)
-
-
-
 
 
 # from setup_data import *
