e28080e9ccde602756cac775727559cf46c96efa
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2019-12-30 02:32:56 -0600

    set default QR

--- logistic/views.py
+++ logistic/views.py
@@ -1,6 +1,9 @@
 from .models import Certificate
 from rest_framework import viewsets
 from .serializers import CertificateSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
 
 # ViewSets define the view behavior.
 class CertificateViewSet(viewsets.ModelViewSet):
@@ -9,3 +12,21 @@ class CertificateViewSet(viewsets.ModelViewSet):
     """
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        uuid = serializer.data['uuid']
+        Certificate.objects.filter(pk=uuid).update(QR='www.aaaimx.org/certificates/?id={0}'.format(uuid))
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def get_success_headers(self, data):
+        try:
+            return {'Location': str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {}
