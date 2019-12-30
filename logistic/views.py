96455273b2849439a175fce55eb6db0a2becf602
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2019-12-30 13:30:14 -0600

    certificates filter

--- logistic/views.py
+++ logistic/views.py
@@ -4,6 +4,7 @@ from .serializers import CertificateSerializer
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
+import re
 
 # ViewSets define the view behavior.
 class CertificateViewSet(viewsets.ModelViewSet):
@@ -12,6 +13,39 @@ class CertificateViewSet(viewsets.ModelViewSet):
     """
     queryset = Certificate.objects.all()
     serializer_class = CertificateSerializer
+
+    def list(self, request):
+        """
+        GET method to process pagination, filtering & sort
+        """
+        # get query params
+        limit = int(request.GET.get('limit', 10))
+        offset = int(request.GET.get('offset', 0))
+        page = int(request.GET.get('page', 1))
+        to = request.GET.get('to', "")
+        type = request.GET.get('type', None)
+
+        self.queryset = self.filter_queryset(self.get_queryset())
+
+        # filter by title
+        matched = list(filter(lambda m: re.findall(
+            to.upper(), m.to.upper()), self.queryset))
+        if type:
+            matched = list(
+                filter(lambda m: m.type == type, matched))
+
+        # pagination
+        queryset = matched[offset:limit*page]
+        page = self.paginate_queryset(matched)
+
+        # serialize data
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
