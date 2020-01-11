596342634b59672235c8dcc2c1a3dd5e74803dee
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-01-10 21:15:48 -0600

    multiple change status

--- logistic/admin.py
+++ logistic/admin.py
@@ -4,10 +4,15 @@ from .models import *
 # Register your models here.
 @admin.register(Certificate)
 class AdminCertifcate(admin.ModelAdmin):
-    list_display = ('uuid', 'type', 'to', 'QR', 'file', 'description')
+    list_display = ('uuid', 'type', 'to', 'QR', 'published', 'file', 'description')
     list_filter = ('type', 'to',)
     search_fields = ('to', 'description')
+    actions = ['publish',]
     list_per_page = 10
+
+    def publish(self, request, queryset):
+        queryset.update(published=True)
+    publish.short_description = "Publish selected certificates"
 
 @admin.register(Event)
 class AdminEvent(admin.ModelAdmin):
