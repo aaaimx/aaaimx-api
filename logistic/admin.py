5709c066a9ab7e501e4930d85f6410b8676ab6f8
Raul Novelo Cruz <rnovelo.cruz98@gmail.com>
2020-06-09 00:21:10 -0500

    Filter by event

--- logistic/admin.py
+++ logistic/admin.py
@@ -2,21 +2,28 @@ from django.contrib import admin
 from .models import *
 from .mixins import ExportCsvMixin
 
+EVENT = "SINABIA 2019"
+
 # Register your models here.
 @admin.register(Certificate)
 class AdminCertifcate(admin.ModelAdmin, ExportCsvMixin):
-    list_display = ('QR', 'type', 'to',  'published',
-                    'created_at', 'file', 'description')
-    list_filter = ('type', 'created_at',)
+    list_display = ('to', 'type', 'event',  'published', 'file', 'description')
+    list_filter = ('event', 'type', 'published', 'created_at',)
     search_fields = ('to', 'description', 'created_at')
     ordering = ('-created_at',)
-    actions = ['publish', 'export_as_csv']
+    actions = ['publish', 'export_as_csv', 'add']
     list_per_page = 10
 
+    def add(self, request, queryset):
+        queryset.update(
+            event=EVENT)
+    
     def publish(self, request, queryset):
         queryset.update(published=True)
-    publish.short_description = "Publish selected certificates"
 
+    add.short_description = "Add a event: " + EVENT
+    publish.short_description = "Publish selected certificates"
+    
 @admin.register(Event)
 class AdminEvent(admin.ModelAdmin):
     ordering = ('-date_start',)
